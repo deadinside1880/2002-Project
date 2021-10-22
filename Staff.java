9606752197b328faec3f783d2bd689d6b627992b
sashank152 <67682093+sashank152@users.noreply.github.com>
2021-10-22 13:54:07 +0800

    Add files via upload

---
+++ Staff.java
@@ -0,0 +1,48 @@
+package orderAndStaff;
+
+public class Staff {
+	
+	private String name;
+	private String  gender;
+	private String employeeID;
+	private String jobTitle;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String names)
+	{
+		this.name=names;
+	}
+	
+	public String getGender() {
+		return gender;
+	}
+	
+	public void setGender(String gen)
+	{
+		this.gender=gen;
+	}
+	
+	public String getEmployeeID() {
+		return employeeID;
+	}
+	
+	public void  setEmployeeID(String EmpID)
+	{
+		this.employeeID=EmpID;
+	}
+	
+	public String getJobTitle()
+	{
+		return jobTitle;
+	}
+	
+	public void setJobTitle(String jobs)
+	{
+		this.jobTitle=jobs;
+	}
+
+
+}
