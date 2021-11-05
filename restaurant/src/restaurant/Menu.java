1028c62138e5a42bcbacdcc909681f1f527d34af
sashank152 <67682093+sashank152@users.noreply.github.com>
2021-11-05 18:07:53 +0800

    Add files via upload

---
+++ restaurant/src/restaurant/Menu.java
@@ -0,0 +1,333 @@
+package restaurant;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.net.URL;
+import java.util.Scanner;
+
+// menu consists of the collection of ala Carte items and promotion items 
+public class Menu {
+
+	public static void main(String[] args) throws IOException {
+		Menu Menu = new Menu(); 
+		Menu.displayMenu();
+	}
+	
+	ArrayList<MenuItems> alaCarteMenu  = new ArrayList<>(); 
+	ArrayList<Promotions> promoMenu = new ArrayList<>(); 
+	
+	private BufferedReader getBR(String filename)throws FileNotFoundException, IOException{
+		//File currentdir = new File(".");
+		//displayDirectoryFiles(currentdir);
+		// File parent = currentdir.getParentFile();
+		// System.out.println(currentdir.getCanonicalPath());
+		// File newf = new File(filename);
+		// System.out.println(newf.exists());
+		// System.out.println(newf.getAbsolutePath());
+		URL url = getClass().getResource(filename);
+ 		File file = new File(url.getPath());
+		FileReader fr=new FileReader( file.getAbsolutePath());   
+ 		BufferedReader br=new BufferedReader(fr);
+		return br;
+	}
+
+//	public static void displayDirectoryFiles(File dir) {
+//		try {
+//			File[] files = dir.listFiles();
+//			for (File file : files) {
+//				if (file.isDirectory()) {
+//					System.out.println("directory:" + file.getCanonicalPath());
+//					displayDirectoryFiles(dir);
+//				} else {
+//					System.out.println("     file:" + file.getCanonicalPath());
+//				}
+//			}
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//	}
+
+	public Menu() throws IOException {
+		alaCarteMenu = readcsvAlaCarte(getBR("MenuItems.csv")); 
+		promoMenu = readCSVPromo(getBR("Promotions.csv"));
+	}
+	
+	public ArrayList<MenuItems> readcsvAlaCarte(BufferedReader br) {
+		ArrayList<MenuItems> alaCarteMenu = new ArrayList<>();
+		
+		try {
+			//BufferedReader br = new BufferedReader(new FileReader(fileName)); 
+			while(br.readLine() != null) {
+				String line = br.readLine(); 
+				// System.out.println(line);
+				while(line != null) {
+					String[] attributes = line.split(","); 
+					
+					MenuItems alaCarteItem = createAlaCarteMenu(attributes); //
+					alaCarteMenu.add(alaCarteItem);
+
+					line = br.readLine();
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		// MenuItems[] alaCarteMenu = (MenuItems[]) alaCarte;
+		return alaCarteMenu; 
+	}
+	
+	public ArrayList<Promotions> readCSVPromo(BufferedReader br) {
+		ArrayList<Promotions> promoMenu = new ArrayList<>();
+		try {
+			//BufferedReader br = new BufferedReader(new FileReader(fileName));
+			while(br.readLine() != null) {
+				String line = br.readLine(); 
+				while(line != null) {
+					String[] attributes = line.split(","); // 0: promoName 1: itemName 2:promoPrice 3: item description 
+					
+					Promotions promoItem = createPromoMenu(attributes); 
+					promoMenu.add(promoItem);
+
+					line = br.readLine();
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return promoMenu; 
+	}
+
+	public MenuItems createAlaCarteMenu(String[] metadata) {
+		// Read file to create the ala carte menu 
+		String name = metadata[0];
+		String description = metadata[1]; 
+		double price = Double.parseDouble(metadata[2]);
+		String type = metadata[3]; 
+		MenuItems alaCarteItem = null; 
+		
+		switch(type) {
+		case "MainCourse": 
+			alaCarteItem = new MainCourse(name, description, price); 
+			break; 
+		case "SideDishes":
+			alaCarteItem = new SideDishes(name, description, price); 
+			break;
+		case "Beverages":
+			alaCarteItem = new Beverages(name, description, price); 
+			break;
+		case "Desserts": 
+			alaCarteItem = new Desserts(name, description, price); 
+			break;
+		default:
+			alaCarteItem = new MenuItems(name, description, price); 
+		}
+		
+		return alaCarteItem;
+	}
+	
+	public Promotions createPromoMenu(String[] metadata) {
+		String promoName = metadata[0]; 
+		String promoDescription = metadata[1]; 
+		double promoPrice = Double.parseDouble(metadata[2]);
+		String promoItems = metadata[3]; 
+		
+		return new Promotions(promoName, promoDescription, promoPrice, promoItems); 
+	}
+	
+	public void addAlaCarteItem(String name, String description, double price) {
+		MenuItems newItem = new MenuItems(name, description, price);
+		alaCarteMenu.add(newItem);
+	}
+	
+	public void addPromotionItem(String name, String description, String promoItems, double price) {
+		Promotions newItem = new Promotions(name, description, price, promoItems);
+		promoMenu.add(newItem);
+	}
+
+	
+	public void updateAlaCarte(String name) {
+		// find the item 
+		int index = -1;
+		Scanner sc = new Scanner(System.in); 
+		for (int i = 0; i < alaCarteMenu.size(); i++) {
+			if (alaCarteMenu.get(i).getName().equals(name.trim()))
+				index = i;
+		}
+		
+		
+		// update the item 
+		if (index == -1)
+			System.out.println("Item cannot be found");
+		else {
+			int [] toEdit = new int[4]; 
+			int choice; 
+			int num = 0; 
+			
+			do {
+				System.out.println("What do you want to edit?");
+				System.out.println("(1) Item Name (2) Item Description (3) Item Price (4) None");
+				System.out.println("Press 5 once you are done with your selection");
+				
+				choice = sc.nextInt();
+				toEdit[num++] = choice;
+				
+			}while (choice < 4 && num < 3);
+			
+			// TO EDITTTTT 
+			sc.nextLine();
+			
+			for (int i = 0; i < toEdit.length; i++) {
+				// System.out.println(toEdit[i]);
+				if (toEdit[i] == 1) {
+					System.out.println("Enter the new Item Name");
+					String newName = sc.nextLine();
+					alaCarteMenu.get(index).setName(newName);
+				}
+				else if (toEdit[i] == 2) {
+					System.out.println("Enter the new item description");
+					String newDescription = sc.nextLine();
+					alaCarteMenu.get(index).setDescription(newDescription);
+				}
+				else if (toEdit[i] == 3) {
+					System.out.println("Enter the new item price"); 
+					double newPrice = sc.nextDouble(); 
+					alaCarteMenu.get(index).setPrice(newPrice);
+				}			
+			}
+			 
+		}
+		
+		// write to the file
+		// System.out.println(index);
+	}
+	
+	public void updatePromo(String name) {
+		// find the item 
+		int index = -1;
+		Scanner sc = new Scanner(System.in); 
+		
+		for (int i = 0; i < promoMenu.size(); i++) {
+			// System.out.println("Entered");
+			if (promoMenu.get(i).getPromoName().trim().equals(name.trim()))
+				index = i;
+		}
+		
+		
+		// update the item 
+		if (index == -1)
+			System.out.println("Item cannot be found");
+		else {
+			
+			int [] toEdit = new int[5]; 
+			int choice; 
+			int num = 0; 
+			
+			do {
+				System.out.println("What do you want to edit?");
+				System.out.println("(1) Promo Name (2) Promo Description (3) Promo Price (4) Promo Items (5) None of the above");
+				System.out.println("Press 5 once you are done with your selection");
+				
+				choice = sc.nextInt();
+				toEdit[num++] = choice; 
+				
+			}while (choice < 5 && num < 4);
+			
+			// TO EDITTTTT 
+			sc.nextLine();
+			
+			for (int i = 0; i < toEdit.length; i++) {
+				// System.out.println(toEdit[i]);
+				if (toEdit[i] == 1) {
+					System.out.println("Enter the new Promo Name");
+					String newName = sc.nextLine();
+					promoMenu.get(index).setPromoName(newName);
+				}
+				else if (toEdit[i] == 2) {
+					System.out.println("Enter the new promo description");
+					String newDescription = sc.nextLine();
+					promoMenu.get(index).setDescription(newDescription);
+				}
+				else if (toEdit[i] == 3) {
+					System.out.println("Enter the new promo price"); 
+					double newPrice = sc.nextDouble(); 
+					promoMenu.get(index).setPackagePrice(newPrice);
+				}	
+				else if (toEdit[i] == 4) {
+					System.out.println("Enter the new promo items"); 
+					String newItem= sc.nextLine(); 
+					promoMenu.get(index).setPromoItems(newItem);
+				}
+			}
+			 
+		}
+
+		// System.out.println(index);
+	}
+	
+
+	
+	public ArrayList<MenuItems> removeAlaCarteItem(String name){
+		int index = -1;
+		for (int i = 0; i < alaCarteMenu.size(); i++) {
+			if (alaCarteMenu.get(i).getName().equals(name)) 
+				index = i; 
+		}
+		 
+		System.out.println(index);
+		if (index != -1)
+			alaCarteMenu.remove(index); 
+		return alaCarteMenu;
+		
+	}
+	
+	public ArrayList<Promotions> removePromoItem(String promoName){
+		int index = -1;
+		for (int i = 0; i < promoMenu.size(); i++) {
+			if (promoMenu.get(i).getPromoName().equals(promoName)) 
+				index = i; 
+		}
+		 
+		System.out.println(index);
+		if (index != -1)
+			promoMenu.remove(index); 
+		return promoMenu;
+		
+	}
+	
+
+	// TO CHECK THAT ALL READ CORRECTLY
+	public void displayMenu() {
+		
+		// Display the alaCarte Menu
+		System.out.println("----------- ALA CARTE MENU ---------------------");
+		for (int i = 0; i < alaCarteMenu.size(); i++) {
+			System.out.printf("%s \t --- \t %.2f \n", alaCarteMenu.get(i).getName(), alaCarteMenu.get(i).getPrice()); 
+			System.out.println(alaCarteMenu.get(i).getDescription());
+			// System.out.println("Type: " + alaCarteMenu.get(i).getType());
+		}
+		System.out.println();
+		
+		System.out.println("----------- PROMOTION MENU ---------------------");
+		// Display the Promotion Menu 
+		for (int i = 0; i < promoMenu.size(); i++) {
+			System.out.printf("[%s] %s \t ----- \t %.2f\n", promoMenu.get(i).getPromoName(), promoMenu.get(i).getPromoItems(), promoMenu.get(i).getPromoPrice()); 
+			System.out.println(promoMenu.get(i).getPromoDescription()); 
+		}
+		System.out.println();
+	}
+	
+	
+}
+
+
+// Pseudo Code
+// Read CSV File to determine size of the file --> Size of the array to be initialised 
+// 1. Read the CSV File to create menu (createMenu Method)
+// 2. Read the CSV File to create promotions (createPromo Method) 
+// 3. Display menu methods
---
+++ restaurant/src/restaurant/Order.java
@@ -0,0 +1,385 @@
+package restaurant;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter; 
+import java.io.PrintWriter;
+import java.net.URL;
+import java.util.ArrayList;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Order{
+	
+	Staff staff=new Staff();
+ 	private boolean membership=false;
+ 	ArrayList<String> neworder = new ArrayList<String>();
+	
+ 	public Staff getStaff() throws IOException
+ 	{
+ 		String empid;
+ 		String s;
+ 		String[] words=null;
+ 		int flag=0;
+ 		
+ 		Scanner sc=new Scanner(System.in);
+ 		System.out.println("Enter the employeeID of the staff making this order");
+		empid=sc.nextLine();
+ 		this.staff.setEmployeeID(empid);
+ 		URL url = getClass().getResource("Staff.csv");
+ 		File file = new File(url.getPath());
+		FileReader fr=new FileReader( file.getAbsolutePath());   
+ 		BufferedReader br=new BufferedReader(fr);   
+ 		while((s=br.readLine())!=null)
+ 		{
+ 			words=s.split(",");
+			
+ 			if(words[0].equals(this.staff.getEmployeeID()))
+ 			{
+ 				this.staff.setName(words[1]);;
+ 				this.staff.setGender(words[2]);
+ 				this.staff.setJobTitle(words[3]);
+ 				flag=1;
+ 				break;
+ 			}
+				
+ 		}
+ 		if(flag==0)
+ 		{
+ 			char mem=' ';
+ 			int flag1=0;
+ 			System.out.println("Staff not found in the database!!!");
+ 			do
+	 		{
+	 		System.out.println("Do you want to add this staff to the database?(y/n)");
+	 		mem=sc.next().charAt(0);
+	 		switch(mem)
+	 		{
+	 		case 'y':
+	 		case 'Y':
+	 			setStaff(empid);
+	 			flag1=1;
+	 			break;
+	 		case 'n':
+	 		case 'N':
+	 			flag1=1;
+	 			break;
+	 		default:
+	 			System.out.println("Please enter y/n");
+	 			break;
+	 		}
+ 		}while(flag1==0);
+ 	 }
+		fr.close();
+		return this.staff;
+ 	}
+ 	
+ 	public void setStaff(String empid) throws IOException
+ 	{
+ 		Scanner sc=new Scanner(System.in);
+ 		System.out.println("Enter the name of the new staff");
+ 		this.staff.setName(sc.nextLine());
+ 		this.staff.setEmployeeID(empid);
+ 		System.out.println("Enter the gender of the new staff");
+ 		this.staff.setGender(sc.nextLine());;
+ 		System.out.println("Enter the jobTitle of the new staff");
+ 		this.staff.setJobTitle(sc.nextLine());
+ 		
+ 		URL url = getClass().getResource("Staff.csv");
+ 		File file = new File(url.getPath());
+ 		FileWriter fw = new FileWriter(file.getAbsolutePath(), true);
+ 		BufferedWriter bw = new BufferedWriter(fw);
+		PrintWriter pw = new PrintWriter(bw);
+ 		pw.println(this.staff.getEmployeeID()+','+this.staff.getName()+','+this.staff.getGender()+','+this.staff.getJobTitle());
+ 		System.out.println("Data Successfully appended into file");
+		pw.flush();
+ 		pw.close();
+	    bw.close();
+	    fw.close();
+ 	}
+
+
+ 	
+ 	public boolean getMembership()
+ 	{
+ 		return membership;
+ 	}
+ 	
+ 	public void setMembership(boolean member)
+ 	{
+ 		if(member == true)
+ 		{
+ 			this.membership=true;
+ 		}
+ 		else
+ 		{
+ 			
+ 			this.membership=false;
+ 		}
+ 	}
+ 	
+ 	public void createOrder() throws IOException
+ 	{
+ 		Scanner sc=new Scanner(System.in);
+ 		String startDate;
+ 		
+ 		int i=0,choice=-1;
+ 		startDate=getTimeAndDate();
+	 	neworder.add(i++,"Order Created At"+startDate);
+	 	staff=getStaff();
+	 	while(staff.getName()==null)
+	 	{
+	 		staff=getStaff();
+	 	}
+	 	neworder.add(i++,"Order created by Staff:"+staff.getName());
+ 		while(choice!=6) 
+ 		{
+ 	 		System.out.println("Please select what you would like to do");
+ 	        System.out.println("(1) Add an ala carte item to the order");
+ 	        System.out.println("(2) Add a promotion item to the order");
+ 	        System.out.println("(3) Remove an item from the order");
+ 	        System.out.println("(4) Cancel the order");
+ 	        System.out.println("(5) View the current Order");
+ 	        System.out.println("(6) Finish the current Order");
+ 	        choice=sc.nextInt();
+ 	        
+ 	        switch(choice)
+ 	        {
+ 	        	case 1:
+ 	        		i=addItem(i);
+ 	        		break;
+ 	        	case 2:
+ 	        		i=addPromo(i);
+ 	        		break;
+ 	        	case 3:
+ 	        		if(removeItem()==1)
+ 	        		{
+ 	        			i=i-1;
+ 	        		}
+ 	        		break;
+ 	        	case 4:
+ 	        		cancelOrder();
+ 	        		choice=6;
+ 	        		break;
+ 	        	case 5:
+ 	        		viewOrder();
+ 	        		break;
+ 	        	case 6:
+ 	        		finishOrder(i);
+ 	        		break;
+ 	        }
+ 		}
+ 	}
+ 	
+ 	public int addItem(int i)throws IOException
+ 	{
+ 		String item,s,price=null;
+ 		String[] words=null;
+ 		int count=0;
+ 		Scanner sc=new Scanner(System.in);
+
+ 		System.out.println("Enter the Item you want to add to the order");
+ 		item=sc.nextLine();
+ 		item=item.toLowerCase();
+ 		URL url = getClass().getResource("MenuItems.csv");
+ 		File file = new File(url.getPath());
+ 		FileReader fr=new FileReader(file);   
+		BufferedReader br=new BufferedReader(fr);  
+		while((s=br.readLine())!=null)
+	 	{
+	 			words=s.split(",");
+				
+	 			if(words[0].toLowerCase().trim().equals(item.trim()))
+	 			{
+	 				price=words[2];
+	 				count=1;
+	 				break;
+	 				
+	 			}
+	 	}
+		if(count==0)
+			{
+				System.out.println("Promotions meal ordered is not in the menu!!! Please try again");
+			}
+		if(count==1)
+			{
+				neworder.add(i,item+','+price);
+				i+=1;
+				System.out.println("Promotion meal has been added to the order!!!");
+			}
+		fr.close();
+		return i;
+ 		
+ 	}
+ 	
+ 	public int addPromo(int i)throws IOException
+ 	{
+ 		String item,s,price=null;
+ 		String[] words=null;
+ 		int count=0;
+ 		Scanner sc=new Scanner(System.in);
+
+ 		System.out.println("Enter the promotion meal you want to add to the order");
+ 		item=sc.nextLine();
+ 		item=item.toLowerCase();
+ 		
+ 		URL url = getClass().getResource("Promotions.csv");
+ 		File file = new File(url.getPath());
+ 		FileReader fr=new FileReader(file);   
+		BufferedReader br=new BufferedReader(fr);  
+		
+		while((s=br.readLine())!=null)
+	 	{
+	 			words=s.split(",");
+				System.out.println(words[0].toLowerCase());
+	 			if(words[0].toLowerCase().trim().equals(item.trim()))
+	 			{
+	 				price=words[2];
+	 				count=1;
+	 				break;
+	 				
+	 			}
+	 	}
+		if(count==0)
+		{
+			System.out.println("Item ordered is not in the menu!!! Please try again");
+		}
+		if(count==1)
+		{
+			neworder.add(i,item+','+price);
+			i+=1;
+			System.out.println("Item has been added to the order!!!");
+		}
+	    fr.close();
+ 		return i;
+ 	}
+ 	
+ 	public int removeItem()
+ 	{
+ 		String item;
+ 		int j=0,flag=0;
+ 		Scanner sc=new Scanner(System.in);
+
+ 		System.out.println("Enter the Item you want to remove from the order");
+ 		item=sc.nextLine();
+ 		
+ 		for(String element: neworder)
+			{
+				if(element.contains(item))
+				{
+					neworder.remove(j);
+					flag=1;
+					System.out.println("Item has been removed from the order!!!");
+					return 1;
+				}
+				j+=1;
+			}
+			if(flag==0)
+			{
+				System.out.println("Item to remove is not in the current order");
+			}
+			return 0;
+	}
+ 	
+ 	public void cancelOrder()
+ 	{
+ 		neworder.clear();
+ 		System.out.println("The order has been canceled!");
+ 		
+ 	}
+ 	
+ 	public void viewOrder()
+ 	{
+ 		String[] words=null;
+ 		if(neworder.size()==2)
+ 		{
+ 			System.out.println("The order is empty!!");
+ 			return;
+ 		}
+ 		System.out.println("The current order is:");
+ 		System.out.printf("%-20s%-20s\n","Item","Price(S$)");
+ 		System.out.println("_ _ _ _ _ _ _ _ _ _ _ _ ");
+ 		for(int i=0;i<neworder.size();i++)
+ 		{
+ 			if(neworder.get(i).contains(","))
+ 			{
+ 			words=neworder.get(i).split(",");
+ 			System.out.printf("%-20s%-20s\n",words[0],words[1]);
+ 			}
+ 		}
+ 	}
+ 	
+ 	public void finishOrder(int i) throws IOException
+ 	{
+
+ 		String endDate=getTimeAndDate();
+ 		char mem=' ';
+ 		int flag=0;
+ 		neworder.add(i,"Order Finished At"+endDate);
+ 		Scanner sc=new Scanner(System.in);
+ 		do
+	 		{
+	 		System.out.println("Are you a member?(y/n)");
+	 		mem=sc.next().charAt(0);
+	 		switch(mem)
+	 		{
+	 		case 'y':
+	 		case 'Y':
+	 			setMembership(true);
+	 			flag=1;
+	 			break;
+	 		case 'n':
+	 		case 'N':
+	 			setMembership(false);
+	 			flag=1;
+	 			break;
+	 		default:
+	 			System.out.println("Please enter y/n");
+	 			break;
+	 		}
+	 		}while(flag==0);
+ 		if(neworder.size()==3)
+ 		{
+ 			cancelOrder();
+ 		}
+ 		
+
+ 		URL url = getClass().getResource("currentOrder.txt");
+ 		File file = new File(url.getPath());
+ 		FileWriter f=new FileWriter(file.getAbsolutePath());   
+ 		BufferedWriter b=new BufferedWriter(f);   
+ 		PrintWriter pw = new PrintWriter(b);  
+ 		for(int j=0;j<neworder.size();j++)
+ 		{
+ 			pw.println(neworder.get(j));
+ 		}
+ 		pw.flush();
+ 		pw.close();
+ 		b.close();
+ 		f.close();
+ 		System.out.println("Your order has been created!!");
+ 		System.out.println("Here is a summary of your order");
+ 		viewOrder();
+ 	}
+ 	
+ 	public String getTimeAndDate()
+ 	{
+ 		LocalDateTime createdDateTime = LocalDateTime.now();
+ 		DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+ 		String formattedCreateDate = createdDateTime.format(myFormatObj);
+ 		return formattedCreateDate;
+ 	}
+ 	
+ 	public static void main(String[] args) throws IOException {
+ 	
+ 		Order order=new Order();
+ 		order.createOrder();
+ 	
+ 	}
+ 	
+}
+	
